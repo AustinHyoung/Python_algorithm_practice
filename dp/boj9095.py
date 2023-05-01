6d7824061a72a85ebd67ac9f0b234a0ea883d56d
hakyoung <jhy2297@naver.com>
2023-05-01 17:58:11 +0900

    1,2,3 합 경우의수

---
+++ dp/boj9095.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+dp = [0] * 100
+
+for i in range(t):
+  n = int(input())
+
+  dp[1] = 1
+  dp[2] = 2
+  dp[3] = 4
+
+  for j in range(4, n+1):
+    dp[j] = dp[j-1] + dp[j-2] + dp[j-3]
+  
+  print(dp[n])
