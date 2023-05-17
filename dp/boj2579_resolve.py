0c24b4123cf767c7e18533b3d068dff7ab915a01
hakyoung <jhy2297@naver.com>
2023-05-17 22:46:19 +0900

    계단 오르기 재풀이 이해

--- dp/boj2579_resolve.py
+++ dp/boj2579_resolve.py
@@ -1,4 +1,3 @@
-# 재풀이
 n = int(input())
 scores = [0] * (n+1)
 dp = [0] * (n+1)
@@ -11,7 +10,6 @@ if n >= 2:
   dp[2] = scores[1] + scores[2]
 
 for i in range(3, n+1):
-  # 한계단씩 가는것과 한계단을 띄어 가는것
   dp[i] = max(dp[i-2] + scores[i], dp[i-3] + scores[i-1] + scores[i])
 
 print(dp[n])
